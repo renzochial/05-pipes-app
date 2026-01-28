de701972ba67d3265d51b8a2a9339569d9335e79
renzochial <renzochia0916@gmail.com>
2026-01-27 21:31:16 -0500

    probando el i18nPluralPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'renzo',
@@ -19,7 +19,7 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page.component',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -38,6 +38,30 @@ export default class UncommonPageComponent {
     }
 
     this.client.set(client1);
+  }
+
+  //i18n Plural
+
+  clientsMap = signal( {
+    '=0' : 'no tenemos ningún cliente esperando',
+    '=1' : 'tenemos un cliente esperando',
+    '=2' : 'tenemos 2 clientes esperando',
+    other : 'tenemos # clientes esperando',
+  })
+
+  clients = signal([
+    'Renzo',
+    'Yoana',
+    'Alejo',
+    'Camilo',
+    'Luciana',
+    'Mery',
+    'Luis',
+    'Pedro',
+  ])
+
+  deleteClient(){
+    this.clients.update(prev => prev.slice(1))
   }
 
 }
