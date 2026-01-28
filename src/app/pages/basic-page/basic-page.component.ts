585efb040de6f5175e869bcf764ebda3597b8d18
renzochial <renzochia0916@gmail.com>
2026-01-27 19:58:59 -0500

    pipes basic, number y dates

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+
+  nameLower = signal('renzo');
+  nameUpper = signal('RENZO');
+  fullName = signal('rEnzO ChIa');
+
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup)=> {
+
+    const interval = setInterval(()=>{
+      this.customDate.set(new Date())
+    }, 1000);
+
+    onCleanup(()=>{
+      clearInterval(interval)
+    })
+
+  })
+
+}
--- src/app/pages/number-page/number-page.component.ts
+++ src/app/pages/number-page/number-page.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './number-page.component.html',
 })
-export default class NumberPageComponent { }
+export default class NumberPageComponent {
+
+    totalSells = signal(2_334_533.5567);
+    percent = signal(0.4856);
+
+ }
