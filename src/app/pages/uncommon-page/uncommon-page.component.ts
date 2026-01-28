4e6d68123e37c313a4c600b9985508244c50d337
renzochial <renzochia0916@gmail.com>
2026-01-27 21:19:02 -0500

    agregando el i18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'renzo',
+  gender: 'male',
+  age: 35,
+  address: 'Villa del rosario, Colombia'
+}
+
+const client2 = {
+  name: 'yoana',
+  gender: 'female',
+  age: 34,
+  address: 'Cucuta, Colombia'
+}
+
 
 @Component({
   selector: 'app-uncommon-page.component',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient(){
+    if(this.client() == client1){
+      this.client.set(client2)
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+}
