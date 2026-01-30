1b4a9847b622b6f3d8eb26a72114a1dd3c9aaac5
renzochial <renzochia0916@gmail.com>
2026-01-29 19:11:53 -0500

    pipes personalizados y tabla dinámica

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+
+    name = signal('Renzo Chia');
+
+    upperCase = signal(true);
+
+    heroes = signal(heroes);
+ }
