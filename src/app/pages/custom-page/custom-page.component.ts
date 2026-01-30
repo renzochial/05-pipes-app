0b4d84eb2f9048df22d1f0489c3647916f476c8c
renzochial <renzochia0916@gmail.com>
2026-01-29 19:41:34 -0500

    se crean los pipes herocreator y herotextcolor

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,12 +1,21 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
+  imports: [
+    ToggleCasePipe,
+    CanFlyPipe,
+    HeroColorPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+    HeroCreatorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
