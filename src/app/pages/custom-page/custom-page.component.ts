184fd41e3e114fcf787c077126e0158b62d93ddc
renzochial <renzochia0916@gmail.com>
2026-01-29 19:58:53 -0500

    se crea el pipe herosortby

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -6,6 +6,8 @@ import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-custom-page',
@@ -15,7 +17,9 @@ import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
     HeroColorPipe,
     HeroTextColorPipe,
     TitleCasePipe,
-    HeroCreatorPipe],
+    HeroCreatorPipe,
+    HeroSortByPipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -25,4 +29,6 @@ export default class CustomPageComponent {
     upperCase = signal(true);
 
     heroes = signal(heroes);
+
+    sortBy = signal<keyof Hero | null>(null);
  }
