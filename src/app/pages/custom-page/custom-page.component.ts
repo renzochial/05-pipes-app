0b0bf1163590a393be304cf48ab852e411c3df0f
renzochial <renzochia0916@gmail.com>
2026-01-29 19:28:38 -0500

    creando canfly y herocolor pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
